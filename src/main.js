e01fdaa0b3f72ba54255d511dc76071e482f0db4
Leon Wang <leon.lw.wang@gmail.com>
2024-09-30 20:11:45 -0500

    migrate to router system

--- src/main.js
+++ src/main.js
@@ -6,10 +6,4 @@ await loadPage('/src/index/profile.html', 'div[index]').then(() => {
   render(document.querySelector('canvas[stippler'))
 })
 
-document.querySelector('div[profile]').innerHTML = `
-  <h3 id="placeholder" style="padding-bottom: 4rem;">[Profile]</h3>
-`
-
-document.querySelector('div[projects]').innerHTML = `
-  <h3 id="placeholder" style="padding-bottom: 4rem;">[Projects]</h3>
-`
+await loadPage('/src/projects/projects.html', 'div[projects]')
