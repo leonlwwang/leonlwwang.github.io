ccea7dff94ce4750199633945e6174cd0a81a841
Leon Wang <leon.lw.wang@gmail.com>
2024-09-30 19:57:06 -0500

    page route test

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import './style.css'
 import { render } from './index/index.js'
+import { loadPage } from './router.js'
 
-document.querySelector('div[index]').innerHTML = `
-  <canvas stippler width='500' height='500'></canvas>
-`
-
-render(document.querySelector('canvas[stippler]'))
+await loadPage('/src/index/profile.html', 'div[index]').then(() => {
+  render(document.querySelector('canvas[stippler'))
+})
 
 document.querySelector('div[profile]').innerHTML = `
   <h3 id="placeholder" style="padding-bottom: 4rem;">[Profile]</h3>
