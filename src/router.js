99b055b1b4d697634da9332a32184f4fa44089e6
Leon Wang <leon.lw.wang@gmail.com>
2024-10-06 22:58:53 -0500

    cls bugfix

--- src/router.js
+++ src/router.js
@@ -5,8 +5,10 @@ export const loadPage = async (url, selector) => {
     if (!response.ok) {
       throw new Error(`Failed to load page ${url}: ${response.statusText}`)
     }
+    console.log(element.innerHTML)
     const html = await response.text()
     element.innerHTML = html
+    console.log(element.innerHTML)
   } catch (e) {
     console.error(`Page retrival failed: ${e}`)
   }
