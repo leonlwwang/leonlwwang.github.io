88cfd4513ff721265c570a2da9f1f0ce6d3bd0ca
Leon Wang <leon.lw.wang@gmail.com>
2024-10-07 21:38:01 -0500

    drag and drop logic

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import '/src/style.css'
 import { colorBtn } from '/src/index/view/button'
 import { render } from '/src/index/index.js'
 import { loadPage } from '/src/router.js'
+import { enableDragDrop } from '/src/index/view/draggable'
 
 await loadPage('/src/index/profile.html', 'div[index]').then(() => {
   colorBtn()
+  enableDragDrop(
+    document.querySelector('img[pow-block]'),
+    document.querySelector('canvas[stippler]')
+  )
   render(document.querySelector('canvas[stippler]'))
 })
 
