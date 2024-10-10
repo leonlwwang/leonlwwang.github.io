78e29a61650f12581c8786ecf2f4d0ebc26e3e05
Leon Wang <leon.lw.wang@gmail.com>
2024-10-10 18:12:41 -0500

    increase velocity cap for bigger bounces

--- src/index/common/math/constants.js
+++ src/index/common/math/constants.js
@@ -4,6 +4,6 @@ export const MOUSE_TIMEOUT = 100
 export const GRAVITY = -1e-4
 export const FRICTION = 0.005
 export const WEIGHT = 100
-export const VELOCITY_MAX = 0.005
+export const VELOCITY_MAX = 0.2
 
 export const UINT_32 = new Uint32Array(1).byteLength
