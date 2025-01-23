571b63551dd0c7a8a8c75c8cae264f8b05d7c7be
Leon Wang <leonw2@illinois.edu>
2025-01-22 22:18:37 -0600

    define gql query

---
+++ src/projects/data/query.js
@@ -0,0 +1,31 @@
+export const query = `
+  query($after: String) {
+    user(login: "leonlwwang") {
+      repositories(first: 9, after: $after, ownerAffiliations: OWNER, orderBy: {field: PUSHED_AT, direction: DESC}) {
+        nodes {
+          name
+          description
+          createdAt
+          pushedAt
+          owner {
+            login
+          }
+          isFork
+          languages(first: 3) {
+            edges {
+              size
+              node {
+                name
+              }
+            }
+            totalSize
+          }
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }
+`
