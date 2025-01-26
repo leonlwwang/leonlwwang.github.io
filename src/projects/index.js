8e52faf27a19721f25d341e30b91ef31826b9e4f
Leon Wang <leonw2@illinois.edu>
2025-01-26 01:00:12 -0600

    draft loaded state

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,13 +1,37 @@
 /** @typedef {import('./data/types').RepositoryStore} RepositoryStore */
 
 /** @param {RepositoryStore} state */
-export const loadProjects = (state, container) => {
+/** @param {Element[]} containers */
+export const loadProjects = (state, containers) => {
   if (state.errors) {
     console.error(`${state.errors.name}: ${state.errors.message}`)
   }
   const projects = state.repositories
   const pageInfo = state.pageInfo
-  console.log(projects)
   console.log(pageInfo)
-  console.log(container)
+
+  projects.forEach((project, i) => fillContainer(containers[i], project))
+}
+
+/** @param {Element} container */
+const fillContainer = (container, data) => {
+  console.log(container, data)
+  clearLoadingStatus(container)
+}
+
+const clearLoadingStatus = (container) => {
+  container.classList.remove('skeleton')
+  container.style.borderColor = 'rgba(0, 0, 0, 0.7)'
+  container.style.backgroundColor = 'white'
+  removeAfter('div[repo]')
+}
+
+const removeAfter = (className) => {
+  const style = document.createElement('style');
+  style.textContent = `
+    ${className}::after {
+      content: none !important;
+    }
+  `;
+  document.head.appendChild(style);
 }
