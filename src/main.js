18aa2b3fce78d8da47e2dbf106927fb535272820
Leon Wang <leonw2@illinois.edu>
2025-02-10 21:02:14 -0600

    fix top level await deploy error

--- src/main.js
+++ src/main.js
@@ -12,19 +12,26 @@ import { loadProjects } from '/src/projects/index'
 const repositoryStore = useRepositoryStore.getState()
 repositoryStore.searchRepositories(query)
 
-await loadPage('/src/index/profile.html', 'div[index]').then(() => {
-  colorBtn()
-  enableDragDrop(
-    document.querySelector('img[pow-block]'),
-    document.querySelector('canvas[stippler]'),
-    enableGravity
-  )
-  enableToolbar(document.querySelector('div[toolbar]'))
-  render(document.querySelector('canvas[stippler]'))
-})
+const renderProfilePage = async () => {
+  await loadPage('/src/index/profile.html', 'div[index]').then(() => {
+    colorBtn()
+    enableDragDrop(
+      document.querySelector('img[pow-block]'),
+      document.querySelector('canvas[stippler]'),
+      enableGravity
+    )
+    enableToolbar(document.querySelector('div[toolbar]'))
+    render(document.querySelector('canvas[stippler]'))
+  })
+}
 
-await loadPage('/src/projects/projects.html', 'div[projects]').then(() => {
-  useRepositoryStore.subscribe((state) =>
-    loadProjects(state, Array.from(document.querySelectorAll('div[repo]')))
-  )
-})
+const renderProjectsPage = async () => {
+  await loadPage('/src/projects/projects.html', 'div[projects]').then(() => {
+    useRepositoryStore.subscribe((state) =>
+      loadProjects(state, Array.from(document.querySelectorAll('div[repo]')))
+    )
+  })
+}
+
+renderProfilePage()
+renderProjectsPage()
