2f0bac5fb89ea05cce93422241ddb1339de7525c
Leon Wang <leonw2@illinois.edu>
2025-02-15 15:59:50 -0600

    remove top level await structure

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import '/src/style.css'
 import { colorBtn } from '/src/index/view/button'
 import { render } from '/src/index/index.js'
-import { loadPage } from '/src/router.js'
 import { enableDragDrop } from '/src/index/view/draggable'
 import { enableGravity } from '/src/index/view/engine'
 import { enableToolbar } from '/src/header/gift'
@@ -12,26 +11,15 @@ import { loadProjects } from '/src/projects/index'
 const repositoryStore = useRepositoryStore.getState()
 repositoryStore.searchRepositories(query)
 
-const renderProfilePage = async () => {
-  await loadPage('/src/index/profile.html', 'div[index]').then(() => {
-    colorBtn()
-    enableDragDrop(
-      document.querySelector('img[pow-block]'),
-      document.querySelector('canvas[stippler]'),
-      enableGravity
-    )
-    enableToolbar(document.querySelector('div[toolbar]'))
-    render(document.querySelector('canvas[stippler]'))
-  })
-}
+colorBtn()
+enableDragDrop(
+  document.querySelector('img[pow-block]'),
+  document.querySelector('canvas[stippler]'),
+  enableGravity
+)
+enableToolbar(document.querySelector('div[toolbar]'))
+render(document.querySelector('canvas[stippler]'))
 
-const renderProjectsPage = async () => {
-  await loadPage('/src/projects/projects.html', 'div[projects]').then(() => {
-    useRepositoryStore.subscribe((state) =>
-      loadProjects(state, Array.from(document.querySelectorAll('div[repo]')))
-    )
-  })
-}
-
-renderProfilePage()
-renderProjectsPage()
+useRepositoryStore.subscribe((state) =>
+  loadProjects(state, Array.from(document.querySelectorAll('div[repo]')))
+)
