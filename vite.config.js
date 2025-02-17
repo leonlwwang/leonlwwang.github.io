333b8e92aedbc649b5a365858b103bbc8f744cd1
Leon Wang <leonw2@illinois.edu>
2025-02-16 22:54:31 -0600

    correct asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/your-repo-name/' : '/',
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
-}
+}))
